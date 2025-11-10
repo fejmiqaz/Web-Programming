1d1196c6f65ce8c4889ba4a4ae080eda765a6457
Fejmi <fejmiqaz@gmail.com>
2025-11-10 15:33:37 +0100

    99% Finished Lab

--- src/main/java/mk/finki/ukim/wp/web/controller/BookReservationController.java
+++ src/main/java/mk/finki/ukim/wp/web/controller/BookReservationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping("/books/reservation")
 public class BookReservationController {
     private final BookReservationService bookReservationService;
     private final BookService bookService;
@@ -23,10 +23,10 @@ public class BookReservationController {
         this.bookService = bookService;
     }
 
-    @PostMapping("/reservation")
-    public String reservation(@RequestParam String readerName, @RequestParam Long selectedBookId, @RequestParam String readerAddress, HttpServletRequest request , @RequestParam Integer numCopies, Model model){
+    @PostMapping
+    public String reservation(@RequestParam String readerName, @RequestParam Long bookId, @RequestParam String readerAddress, HttpServletRequest request , @RequestParam Integer numCopies, Model model){
 
-        Book selectedBook = bookService.findById(selectedBookId);
+        Book selectedBook = bookService.findById(bookId);
 
         bookReservationService.placeReservation(selectedBook.getTitle(), readerName, readerAddress, numCopies);
 
@@ -35,6 +35,8 @@ public class BookReservationController {
         model.addAttribute("numCopies", numCopies);
         model.addAttribute("bookTitle", selectedBook.getTitle());
         model.addAttribute("ipAddress", request.getRemoteAddr());
+
+        System.out.println("Book reservation POST received!");
 
         return "reservationConfirmation";
     }
